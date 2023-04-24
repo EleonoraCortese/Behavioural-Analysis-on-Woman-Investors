434061eb88dbfc7499fddb73b863785e9f197111
whiterabbit-97 <69982374+whiterabbit-97@users.noreply.github.com>
2023-04-24 11:45:44 +0200

    Update Exploratory Analysis and Comparison between Male and Female Customers.py

--- Exploratory Analysis and Comparison between Male and Female Customers.py
+++ Exploratory Analysis and Comparison between Male and Female Customers.py
@@ -272,10 +272,6 @@ dff = df1[df1['SESSO'] == "F"]
 #Creating a dataframe from the original one which contains only male customers
 dfm = df1[df1['SESSO'] == "M"]  
 
-
-# In[118]:
-
-
 #Creating a list contaning all the numerical variables
 numericv = list(df1.select_dtypes(include=['float64']).columns)
 numericv.append('CLASSI_ETA')
@@ -284,24 +280,12 @@ numericv.append('PROPENSIONE_AL_RISCHIO')
 #Creating a list containing the wealth allocated by customers on each product 
 patrimoniopdt=[i for i in numericv if i.startswith("PATRIMONIO_")]
 
-
-# In[119]:
-
-
 #Creating a list containing all the categorical variables
 columns = list(df1.columns)
 categoricv = [i for i in columns if i not in numericv]
 
-
-# In[120]:
-
-
 #printing main descriptive statistics for the numerical variables in the male customers' dataset
 dfm[numericv].describe() 
-
-
-# In[121]:
-
 
 #Calculating the percentages of wealth allocated by male customers on each product-macrocategory
 Lm=dfm['LIQUIDITA'].mean()*100/dfm["PATRIMONIO"].mean()
@@ -315,39 +299,22 @@ print(f'Investimento:{round(Im,4)}%')
 #printing the average wealth for the male customers
 print(f'Patrimonio totale:{round(dfm["PATRIMONIO"].mean(),4)}')
 
-
-# In[122]:
-
-
 # creating a donut chart representing the percentages allocated by male customers on each protuct macrocategory
-
 labels = 'LIQUIDITÁ', 'RISPARMIO', 'INVESTIMENTO'
 sizes = [Lm, Rm, Im]
 colors=['#b3d1ff',"#0047b3",'#1a75ff']
-
 fig1, ax1 = plt.subplots()
-
 ax1.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90, textprops={'fontsize': 30})
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
 # draw the inner circle
 centre_circle = plt.Circle((0, 0), 0.80, fc='white')
-
 #Inserting the average wealth in the center
 plt.text(0, 0, "69Mila €", ha='center', va='center', fontsize=42)
 fig1 = plt.gcf()
-
-
 # Adding the inner circle in Pie chart
 fig1.gca().add_artist(centre_circle)
-
 plt.title('UOMINI', fontsize=30)
-
 plt.show()
-
-
-# In[123]:
-
 
 #Creating a Dataframe from the previous one which contains only male investors
 dfminv=dfinv[dfinv['SESSO']=="M"]
@@ -362,45 +329,29 @@ print(percinvm)
 #creating a barplot representing the percentages allocated by male investors on each investment product   
 labels = pdt_inv
 sizes = percinvm
-
 plt.rcParams["figure.figsize"] = [17.00, 11.50]
 plt.rcParams["figure.autolayout"] = True
 plt.title("PERCENTUALI DI ALLOCAZIONE DEL PATRIMONIO")
 plt.ylabel('% Di patrimonio allocato', fontsize=15)
 plt.xlabel('Prodotti d\'investimento',fontsize=15)
-
-
 plt.bar(labels, sizes)
-
 plt.show()
-
-
-# In[124]:
-
 
 #creating a barplot showing the number of male customers in the sample for each digital cluster
 ax = dfm['CLUSTER_DIGITAL'].value_counts().plot(kind='bar', title ="Digitalizzazione della clientela femminile", figsize=(15, 10), legend=True, fontsize=12)
 ax.set_xlabel("cluster digital di appartenenza", fontsize=12)
 ax.set_ylabel("count", fontsize=12)
+
 #calculating the percentages of male customers in the sample for each digital cluster
 print(dfm['CLUSTER_DIGITAL'].value_counts()/dfm.shape[0]*100)
 
-
-# In[125]:
-
-
 #printing main descriptive statistics for the numerical variables in the female customers' dataset
 dff[numericv].describe()
-
-
-# In[126]:
-
 
 #Calculating the percentages of wealth allocated by female customers on each product-macrocategory
 Lf=dff['LIQUIDITA'].mean()*100/dff["PATRIMONIO"].mean()
 Rf=dff['RISPARMIO'].mean()*100/dff["PATRIMONIO"].mean()
 If=dff['INVESTIMENTO'].mean()*100/dff["PATRIMONIO"].mean()
-
 #Printing the resulting percentages
 print(f'liquidità:{round(Lf,4)}%')
 print(f'risparmio:{round(Rf,4)}%')
@@ -409,36 +360,22 @@ print(f'Investimento:{round(If,4)}%')
 #printing the average wealth for the female customers
 print(f'Patrimonio totale:{round(dff["PATRIMONIO"].mean(),4)}')
 
-
-# In[127]:
-
-
 #Creating a donut chart showing the percentages allocated by female customers on each product macrocategory
 labels = 'LIQUIDITÁ', 'RISPARMIO', 'INVESTIMENTO'
 sizes = [Lf, Rf, If]
 colors=['#ffcc99','#cc6600','#ff9933']
-
 fig1, ax1 = plt.subplots()
-
 ax1.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90, textprops={'fontsize': 30})
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 # draw the inner circle
 centre_circle = plt.Circle((0, 0), 0.80, fc='white')
 fig1 = plt.gcf()
-
 #Adding the average wealth for female customers in the middle of the chart
 plt.text(0, 0, "65Mila€", ha='center', va='center', fontsize=42)
-
 # Adding the inner Circle in Pie chart
 fig1.gca().add_artist(centre_circle)
-
 plt.title('DONNE', fontsize=30)
-
 plt.show()
-
-
-# In[128]:
-
 
 #creating a new dataframe from the previous one containing only female investors
 dffinv=dfinv[dfinv['SESSO']=='F']
@@ -453,21 +390,13 @@ print(percinvf)
 #creating a barplot showing the percentages allocated by female investors on each investment product
 labels = pdt_inv
 sizes = percinvf
-
 plt.rcParams["figure.figsize"] = [17.00, 11.50]
 plt.rcParams["figure.autolayout"] = True
 plt.title("PERCENTUALI DI ALLOCAZIONE DEL PATRIMONIO INVESTITO")
 plt.ylabel('% DI PATRIMONIO INVESTITO')
 plt.xlabel('PRODOTTI')
-
-
 plt.bar(labels, sizes)
-
 plt.show()
-
-
-# In[129]:
-
 
 #creating a barplot showing the number of female customers for each digital cluster
 ax = dff['CLUSTER_DIGITAL'].value_counts().plot(kind='bar', title ="Digitalizzazione della clientela femminile", figsize=(15, 10), legend=True, fontsize=12)
@@ -477,19 +406,11 @@ ax.set_ylabel("count", fontsize=12)
 #printing the percentage of female customers for each digital cluster
 print(dff['CLUSTER_DIGITAL'].value_counts()/dff.shape[0]*100)
 
-
-# In[130]:
-
-
 #Printing the percentage on multibank and not-multibank female customers in the sample
 print(dff.FL_MULTIBANCA.value_counts()/dff.shape[0]*100)
 
 #Printing the percentage on multibank and not-multibank male customers in the sample
 print(dfm.FL_MULTIBANCA.value_counts()/dfm.shape[0]*100)
-
-
-# In[131]:
-
 
 #printing the rounded average wealth for female customers
 print(round(dff['PATRIMONIO'].mean(),2))
@@ -497,35 +418,14 @@ print(round(dff['PATRIMONIO'].mean(),2))
 #printing the rounded average wealth for male customers
 print(round(dfm['PATRIMONIO'].mean(),2))
 
-
-# In[132]:
-
-
 #Comparing percentages allocated by male and female investors on investment products 
 print('Confronto percentuali allocazione prodotti:\n')
 for i in pdt_inv:
     diff =(dfminv[i].mean()*100/dfminv['INVESTIMENTO'].mean())-(dffinv[i].mean()*100/dffinv['INVESTIMENTO'].mean())
     print(f'{i}: {round(diff,4)}%')
 
-
-# Da una prima analisi emerge come in percentuale uomini e donne allochino il proprio patrimonio in modo similare sui differenti prodotti di investimento.
-# 
-# Fondi, RamoIII e Multiramo sono i prodotti a cui corrisponde un maggior rischio e un maggior rendimento. Notiamo che la percentuale del patrimonio dedicata a tali prodotti è in media leggermente più elevata per gli uomini.
-# In percentuale la quota allocata dalle donne è in media superiore per la Ramo I e la ramo IV, prodotti a cui è associato un basso livello di rischio e rendimento.
-
-# In[133]:
-
-
 #Comparing the averages values of numerical features in male and female customers' dataframes 
 round(dfm[numericv].mean()-dff[numericv].mean(),4)
-
-
-# Confrontando gli importi medi allocati notiamo alcune differenze imputabili però al patrimonio (superiore negli uomini rispetto alle donne)
-
-# grafici di confronto delle percentuali uomini vs donne
-
-# In[134]:
-
 
 #Creating a multiple barplot showing the percentages allocated by male and female investors on each investment product
 figure2 = plt.bar(pdt_inv, percinvm, align='edge', width=-0.4, label='Uomini')
@@ -538,10 +438,6 @@ plt.rcParams["figure.figsize"] = [25.00, 11.50]
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
 plt.show()
-
-
-# In[135]:
-
 
 #Printing the percentages of female investors by risk-propensity
 print(f"investitori:\n{dfminv['PROPENSIONE_AL_RISCHIO'].value_counts()/dfminv.shape[0]*100}")
@@ -564,23 +460,11 @@ plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
 plt.show()
 
-
-# In[136]:
-
-
 #printing the number of male investors by risk propensity
 dfminv['PROPENSIONE_AL_RISCHIO'].value_counts()/dfminv.shape[0]*100
 
-
-# In[137]:
-
-
 #printing the number of female investors by risk propensity
 dffinv['PROPENSIONE_AL_RISCHIO'].value_counts()/dffinv.shape[0]*100
-
-
-# In[138]:
-
 
 #Creating a multiple barplot showing the percentage of male and female customers in each digital cluster
 uomini = [41.599758,8.159603,18.595085,28.306900,3.338655]
@@ -594,37 +478,22 @@ plt.xlabel('CLUSTER DIGITAL',fontsize=15 )
 plt.ylabel('% SAMPLE',fontsize=15)
 plt.show()
 
-
-# In[139]:
-
-
 #creating a multiple barplot showing the percentage of male and female customers for each age class
 uomini = [2.735017, 8.13047, 11.878154, 17.031219, 20.292963, 20.212555, 19.719623]
 donne = [1.802307, 5.882456, 10.643068, 18.067731, 22.14175, 20.50934, 20.953348]
-
 cluster = ['18-24','25-34','35-44','45-54','55-64','65-74','75+']
-
 figure2 = plt.bar(cluster, uomini, align='edge', width=-0.4, label='Uomini')
 figure1 = plt.bar(cluster, donne, align='edge', width=0.4, label='Donne')
 plt.legend(fontsize=30)
 plt.title("CLASSI D'\ETÁ VS SESSO",fontsize=15)
 plt.ylabel('% SAMPLE',fontsize=15)
 plt.xlabel('CLASSI D\'ETÁ',fontsize=15)
-
 plt.show()
-
-
-
-# In[140]:
-
 
 #calculating and printing the average age for female customers
 etaf_count=list(dff.CLASSI_ETA.value_counts())
-
 valf_centr=[(55+64)/2,(65+74)/2,(75+90)/2,(45+54)/2,(35+44)/2,(25+34)/2,(18+24)/2]
-
 ponderaf = list(zip(valf_centr,etaf_count))
-
 ponderataf=[]
 for i in ponderaf:
     ponderataf.append((i[0]*i[1])/dff.shape[0])
@@ -632,33 +501,18 @@ print(f'età media donne: {round(sum(ponderataf),2)}')
 
 #calculating and printing the average age for male customers
 etam_count=list(dfm.CLASSI_ETA.value_counts())
-
 valm_centr=[(55+64)/2,(65+74)/2,(75+90)/2,(45+54)/2,(35+44)/2,(25+34)/2,(18+24)/2]
-
 ponderam = list(zip(valm_centr,etam_count))
-
 ponderatam=[]
 for i in ponderam:
     ponderatam.append((i[0]*i[1])/dfm.shape[0])
 print(f'età media uomini: {round(sum(ponderatam),2)}')
 
-
-# In[142]:
-
-
 #calculating the percentages of male customers for each geographic area
 (dfm['AREA_TERRITORIALE'].value_counts()/dfm.shape[0]*100)
 
-
-# In[143]:
-
-
 #calculating the percentages of female customers for each geographic area
 dff['AREA_TERRITORIALE'].value_counts()/dff.shape[0]*100
-
-
-# In[144]:
-
 
 #creating a multiple barplot showing the percentages of male and female customers for each geographic area
 uomini = (17.016070,20.832508,26.733712,16.518476,10.307296,8.591938)
@@ -674,12 +528,7 @@ plt.xlabel('MACRO AREE TERRITORIALI',fontsize=15)
 plt.ylabel('% SAMPLE',fontsize=15)
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
-
 plt.show()
-
-
-# In[145]:
-
 
 #printing the total wealth for female customers
 print(dff['PATRIMONIO'].sum())
@@ -687,33 +536,19 @@ print(dff['PATRIMONIO'].sum())
 #printing the total wealth for male customers
 print(dfm['PATRIMONIO'].sum())
 
-
-# In[205]:
-
-
 #creating a barplot showing the total amount invested by female customers on each investment product
 totpatrimonioinv=[] 
 for i in pdt_inv:
     totpatrimonioinv.append(dff[i].sum())
-
- 
 labels = pdt_inv
 sizes = totpatrimonioinv
-
 plt.rcParams["figure.figsize"] = [17.00, 11.50]
 plt.rcParams["figure.autolayout"] = True
 plt.title("ALLOCAZIONE DEL PATRIMONIO INVESTITO")
 plt.ylabel('PATRIMONIO IN €')
 plt.xlabel('PRODOTTI')
-
-
 plt.bar(labels, sizes)
-
 plt.show()
-
-
-# In[201]:
-
 
 #calculating the total amount allocated by female customers of each product of the macrocategory "liquidity"
 totpatrimonioliq=[] 
@@ -725,10 +560,6 @@ totpatrimoniorisp=[]
 for i in pdt_risp:
     totpatrimoniorisp.append(dff[i].sum())
 
-
-# In[153]:
-
-
 #Creating a multiple bar plot showing the distribution of the risk-propensity variable across different age groups ("CLASSI_ETA") for female investors
 ax = dffinv.groupby(['CLASSI_ETA']).PROPENSIONE_AL_RISCHIO.value_counts().reset_index(name='counts').pivot(index='CLASSI_ETA', columns='PROPENSIONE_AL_RISCHIO', values='counts').plot(kind='bar',width = 0.9)
 ax.set_xlabel("CLASSI D'ETÀ", fontsize=15)
@@ -739,21 +570,12 @@ plt.rcParams["figure.figsize"] = [17.00, 11.50]
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
 
-
-# In[154]:
-
-
 #Creating a bar plot showing the distribution of the average wealth variable across different age groups ("CLASSI_ETA") for female investors
 ax1 = dffinv.groupby('CLASSI_ETA')['PATRIMONIO'].mean().plot(kind='bar', title ="PATRIMONIO MEDIO VS CLASSI D'ETÁ", figsize=(15, 10), fontsize=12)
 ax1.set_xlabel("CLASSI D'ETÁ", fontsize=12)
 ax1.set_ylabel("PATRIMONIO IN €", fontsize=12)
 
-
-# In[155]:
-
-
 #Creating a multiple bar plot showing the distribution of the age groups variable across different digital clusters for female investors
-
 ax = dffinv.groupby(['CLASSI_ETA']).CLUSTER_DIGITAL.value_counts().reset_index(name='counts').pivot(index='CLASSI_ETA', columns='CLUSTER_DIGITAL', values='counts').plot(kind='bar',width = 0.9)
 ax.set_xlabel("CLASSI D'ETÀ", fontsize=15)
 ax.set_ylabel("COUNTS", fontsize=15)
@@ -762,10 +584,6 @@ plt.legend(fontsize=20)
 plt.rcParams["figure.figsize"] = [17.00, 11.50]
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
-
-
-# In[72]:
-
 
 #Creating a multiple bar plot showing the distribution female investors across different age groups.
 ax = dff.groupby(['CLASSI_ETA']).FL_INVESTIMENTO.value_counts().reset_index(name='counts').pivot(index='CLASSI_ETA', columns='FL_INVESTIMENTO', values='counts').plot(kind='bar',width = 0.9)
@@ -777,28 +595,15 @@ plt.rcParams["figure.figsize"] = [17.00, 11.50]
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
 
-
-# In[73]:
-
-
 #creating a list containing product associated to high financial risk
 pdt_rischiosi=["PATRIMONIO_FONDI",'PATRIMONIO_RAMO_III','PATRIMONIO_MULTIRAMO']
 #creating a list containing product associated to low financial risk
 pdt_nonrischiosi=['PATRIMONIO_TITOLI','PATRIMONIO_RAMO_I','PATRIMONIO_RAMO_IV','PATRIMONIO_PREVIDENZA','PATRIMONIO_PERSONA']
 
-
-# In[74]:
-
-
 #creating a new variable representing the total ammount allocated by each male investor on high financial risk's product
 dfminv['PATRIMONIO_RISCHIO']=dfminv['PATRIMONIO_FONDI']+dfminv["PATRIMONIO_RAMO_III"]+dfminv["PATRIMONIO_MULTIRAMO"]
-
 #creating a new variable representing the total ammount allocated by each male investor on low financial risk's product
 dfminv['PATRIMONIO_NON_RISCHIO']=dfminv["PATRIMONIO_TITOLI"]+dfminv["PATRIMONIO_RAMO_I"]+dfminv['PATRIMONIO_RAMO_IV']+dfminv["PATRIMONIO_PREVIDENZA"]+dfminv['PATRIMONIO_PERSONA']
-
-
-# In[85]:
-
 
 #calculating the total ammount allocated by male investors on high and low risk products
 m_tot_r = dfminv['PATRIMONIO_RISCHIO'].sum()
@@ -818,18 +623,10 @@ m_tot_perc_nr = m_tot_nr/dfminv['INVESTIMENTO'].sum()*100
 print(m_tot_perc_r)
 print(m_tot_perc_nr)
 
-
-# In[76]:
-
-
 #creating a new variable representing the total ammount allocated by each female investor on high financial risk's product
 dffinv['PATRIMONIO_RISCHIO']=dffinv['PATRIMONIO_FONDI']+dffinv["PATRIMONIO_RAMO_III"]+dffinv["PATRIMONIO_MULTIRAMO"]
 #creating a new variable representing the total ammount allocated by each female investor on low financial risk's product
 dffinv['PATRIMONIO_NON_RISCHIO']=dffinv["PATRIMONIO_TITOLI"]+dffinv["PATRIMONIO_RAMO_I"]+dffinv['PATRIMONIO_RAMO_IV']+dffinv["PATRIMONIO_PREVIDENZA"]+dffinv['PATRIMONIO_PERSONA']
-
-
-# In[77]:
-
 
 #calculating the total ammount allocated by female investors on high and low risk products
 f_tot_r = dffinv['PATRIMONIO_RISCHIO'].sum()
@@ -849,24 +646,11 @@ f_tot_perc_nr = f_tot_nr/dffinv['INVESTIMENTO'].sum()*100
 print(f_tot_perc_r)
 print(f_tot_perc_nr)
 
-
-# In[78]:
-
-
 #calculating the number of female investors for each age class
 dffinv['CLASSI_ETA'].value_counts()
-
-
-# In[79]:
-
 
 #calculating the number of female investors for each geographic area
 dffinv['AREA_TERRITORIALE'].value_counts()
 
-
-# In[80]:
-
-
 #calculating the average investment for female investors
 dffinv['INVESTIMENTO'].mean()
-
